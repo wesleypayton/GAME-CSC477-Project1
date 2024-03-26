ea91354206be6984c833043d1060d6236f3aa762
Eli Payton <80854811+wesleypayton@users.noreply.github.com>
2024-03-26 01:18:20 -0500

    Added background music

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,24 +5,35 @@ using UnityEngine;
 public class Menu : MonoBehaviour {
     public GameObject mainMenu;
     public GameObject gameoverMenu;
+    public AudioSource menuMusic;
+    public AudioSource gameMusic;
 
     void Start() {
+        menuMusic = GameObject.FindGameObjectWithTag("MenuMusic").GetComponent<AudioSource>();
+        gameMusic = GameObject.FindGameObjectWithTag("GameMusic").GetComponent<AudioSource>();
+        menuMusic.Play();
         mainMenu.SetActive(true);
         gameoverMenu.SetActive(false);
         Game.Instance.input.Disable();
     }
 
     public void StartGame() {
+        menuMusic.Stop();
+        gameMusic.Play();
         mainMenu.SetActive(false);
         Game.Instance.input.Enable();
     }
 
     public void GameOver() {
+        gameMusic.Stop();
+        menuMusic.Play();
         gameoverMenu.SetActive(true);
         Game.Instance.input.Disable();
     }
 
     public void Restart() {
+        menuMusic.Stop();
+        gameMusic.Play();
         gameoverMenu.SetActive(false);
         Game.Instance.input.Enable();
     }
